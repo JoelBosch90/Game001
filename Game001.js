642e4ce81bbf747b028e144c50079351a13856c0
Joël Bosch <microsoft@joelbosch.nl>
2018-09-22 18:04:15 +0200

    Move around!

--- Game001.js
+++ Game001.js
@@ -1,28 +1,23 @@
 /* This is where we're painting the canvas like we're Rembrandt. Or rather, more like Rothko or Pollock... */
 
-var CANVAS_WIDTH = 480;
-var CANVAS_HEIGHT = 320;
+let CANVAS_WIDTH = 480;
+let CANVAS_HEIGHT = 320;
+let canvasElement = $("<canvas width='" + CANVAS_WIDTH + 
+                      "' height='" + CANVAS_HEIGHT + "'></canvas>");
 let canvas;
 
 $(document).ready(function() {
-    canvas = document.getElementById("Game001").getContext("2d");
+    canvas = canvasElement.get(0).getContext("2d");
+    canvasElement.appendTo('body');
 
-    var FPS = 60;
+    let FPS = 60;
     setInterval(function() {
         update();
         draw();
     }, 1000/FPS);
-
-    // context.fillRect(0, 0, canvas.width, canvas.height);
-    // context.beginPath();
-    // context.moveTo(0, 0);
-    // context.lineTo(250, 500);
-    // context.strokeStyle = "red";
-    // context.rect(20,20,150,100);
-    // context.stroke();
 });
 
-var player = {
+let player = {
     color: "#00A",
     x: 220,
     y: 270,
@@ -36,10 +31,17 @@ var player = {
 
 let arrowRight = false;
 let arrowLeft = false;
-
+let arrowDown = false;
+let arrowUp = false;
 
 document.onkeydown = function (e) {
     switch (e.key) {
+        case 'ArrowUp':
+            arrowUp = true;
+            break;
+        case 'ArrowDown':
+            arrowDown = true;
+            break;
         case 'ArrowLeft':
             arrowLeft = true;
             break;
@@ -49,8 +51,19 @@ document.onkeydown = function (e) {
 };
 
 document.onkeyup = function (e) {
-    arrowRight = false;
-    arrowLeft = false;
+    switch (e.key) {
+        case 'ArrowUp':
+            arrowUp = false;
+            break;
+        case 'ArrowDown':
+            arrowDown = false;
+            break;
+        case 'ArrowLeft':
+            arrowLeft = false;
+            break;
+        case 'ArrowRight':
+            arrowRight = false;
+    }
 };
 
 function update() {
@@ -61,12 +74,29 @@ function update() {
         player.x -= 2;
     }
 
+    if (arrowDown) {
+        player.y += 2;
+    }
+
     if (arrowRight) {
         player.x += 2;
     }
+
+    if (arrowUp) {
+        player.y -= 2;
+    }
+
+    player.x = player.x.clamp(0, CANVAS_WIDTH - player.width);
+    player.y = player.y.clamp(0, CANVAS_HEIGHT - player.height);
 }
 
 function draw() {
     canvas.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    canvas.fillStyle = "#FFF";
+    canvas.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     player.draw();
 }
+
+Number.prototype.clamp = function(min, max) {
+    return Math.min(Math.max(this, min), max);
+  };
