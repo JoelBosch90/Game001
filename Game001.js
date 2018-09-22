02893567218d56ab8ef35229ee1950d3f03a6ca9
Joël Bosch <microsoft@joelbosch.nl>
2018-09-22 16:44:12 +0200

    Included Jquery!

--- Game001.js
+++ Game001.js
@@ -1,7 +1,10 @@
 /* This is where we're painting the canvas like we're Rembrandt. Or rather, more like Rothko or Pollock... */
 
+$(document).ready(function() {
+
 let canvas = document.getElementById("Game001");
 let context = canvas.msGetInputContext("2D");
 
 context.moveTo(0, 0);
 context.lineTo(250, 500);
+});
