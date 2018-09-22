4650de76ad6446281a415159b29e0cc85a8976cd
Joël Bosch <microsoft@joelbosch.nl>
2018-09-22 16:36:49 +0200

    First line!

---
+++ Game001.js
@@ -0,0 +1,7 @@
+/* This is where we're painting the canvas like we're Rembrandt. Or rather, more like Rothko or Pollock... */
+
+let canvas = document.getElementById("Game001");
+let context = canvas.msGetInputContext("2D");
+
+context.moveTo(0, 0);
+context.lineTo(250, 500);
